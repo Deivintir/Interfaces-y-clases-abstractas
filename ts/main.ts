8ec0846626b229c39b11ed8bfc0c7dae462213e2
Deivintir <d4vidyeva@gmail.com>
2024-02-09 12:01:25 +0100

    Interfaces y clases abstractas

---
+++ ts/main.ts
@@ -0,0 +1,11 @@
+/**Interfaces y clases abstractas:
+ * En esta undidad llegamos a uno de los aspectos más importantes de TypeScript y de cualquier lenguaje de programación orientada a objetos:
+ * el uso de interfaces, clases e instancias de objetos.
+ * Comenaaremos de manera práctica por las interfaces, un secillo mecanismo muy utilizado en TypeScript por la particularidad de las aplicaciones
+ * desarrolladas en javaScript, en las que en muhcas ocasiones la simplicidad y la flexibilidad son necesarias.
+ * Posteriormente, aprenderemos cómo se implementan las clases en TypeScript, cómo se estableec el acceso a sus miembros y cómo es posible crear
+ * miembros estáticos para modularizar nuestro código.
+ * Para concluir, estudiaremos de forma práctica las clases abstractas como complemento al resto de las sintaxis empleadas en el uso de objetos en 
+ * TypeScripty, por tanto, en JavaScript. */
+
+
